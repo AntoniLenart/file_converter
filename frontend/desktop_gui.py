b52f777505ec686d3ba8b2d98c059d243ff2c356
Kindly000 <kuba.kogut.2000@gmail.com>
2024-10-31 16:54:02 +0100

    poprawa wyglądu GUI (desktop_gui.py)

--- frontend/desktop_gui.py
+++ frontend/desktop_gui.py
@@ -2,6 +2,15 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 from backend import converter, file_validator
 
+def center_window(width, height):
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    
+    x = (screen_width // 2) - (width // 2)
+    y = (screen_height // 2) - (height // 2)
+    
+    root.geometry(f"{width}x{height}+{x}+{y}")
+
 def open_file():
     file_path = filedialog.askopenfilename()
     valid, message = file_validator.validate_file(file_path)
@@ -20,14 +29,23 @@ def open_file():
 
 root = tk.Tk()
 root.title("Konwerter plików")
+root.resizable(False, False)
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+center_window(250, 250)
+
+frame = tk.Frame(root, padx=10, pady=10) 
+frame.pack(anchor="center", side="top", pady=65)
+
+tk.Label(frame, text="Wybierz format:").grid(row=0, column=0, pady=(0, 5))
 
 format_var = tk.StringVar(value="jpeg")
 formats = ["jpeg", "png", "gif", "mp3", "wav", "flac", "mp4", "mov"]
+format_menu = tk.OptionMenu(frame, format_var, *formats)
+format_menu.grid(row=1, column=0, pady=(0, 10))
 
-tk.Label(root, text="Wybierz format:").pack()
-format_menu = tk.OptionMenu(root, format_var, *formats)
-format_menu.pack()
-
-tk.Button(root, text="Wybierz plik", command=open_file).pack()
+tk.Button(frame, text="Wybierz plik", command=open_file).grid(row=2, column=0)
 
 root.mainloop()
